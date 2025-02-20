f9e6b78f3220e23823f59397740262a9728721ed
Sahil Naik <nsahil992@gmail.com>
2025-02-20 11:38:14 +0530

    updated

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install cowsay -y
-cowsay -f dog "I am a Dog....Woof Woof!" >> dog.txt
-grep -i "Woof" dog.txt
-cat dog.txt
+cowsay -f longcat "I am a Cat....Meow!" >> cat.txt
+grep -i "Meow" cat.txt
+cat cat.txt
 ls -a
